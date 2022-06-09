5ab3b5a37f8c786ba009b2bf340c59eeb8662f3d
Maximiliano Duva <maduva@microsoft.com>
2022-06-08 21:39:48 -0300

    Habilito swagger ui para release.

--- PartsUnlimitedApi/Program.cs
+++ PartsUnlimitedApi/Program.cs
@@ -6,6 +6,7 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+//https://localhost:<port>/swagger
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddDbContext<PartsUnlimitedContext>(ServiceLifetime.Scoped);
@@ -13,11 +14,11 @@ builder.Services.AddDbContext<PartsUnlimitedContext>(ServiceLifetime.Scoped);
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+//if (app.Environment.IsDevelopment())
+//{
+app.UseSwagger();
+app.UseSwaggerUI();
+//}
 
 app.UseHttpsRedirection();
 
